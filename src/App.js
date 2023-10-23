f3d51ec8ee474963479a6c7e9a0129c71fae0e9f
Hasnain <hasnain10987@gmail.com>
2023-10-24 00:05:59 +0500

    Pre loader add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Header from './components/Header';
+import PreLoader from './components/PreLoader';
 
 
 
 function App() {
   return (
+    <>
     
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Header></Header>} />
     </Routes>
     </BrowserRouter>
-    
+    </>
   );
 }
 
---
+++ src/components/PreLoader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function PreLoader() {
+  return (
+    <div>
+       {/* Preloader */}
+  <div className="preloader">
+    <div className="preloader-inner">
+      <div className="preloader-icon">
+        <span />
+        <span />
+      </div>
+    </div>
+  </div>
+  {/* End Preloader */}
+    </div>
+  )
+}
+
+export default PreLoader
